b9193150820f8d54d5b615ea6a259050b2731b21
Jamal El Milahi <jamal@elmilahi.com>
2013-12-03 10:41:35 -0800

    Show command state in the prompt

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -2,45 +2,27 @@ RED=31
 GREEN=32
 YELLOW=33
 
-function git_repo {
+git_repo() {
   [[ `git rev-parse --is-inside-work-tree 2>/dev/null` == true ]]
 }
 
-function git_modified {
+git_modified() {
   [[ `git ls-files -m` != '' ]]
 }
 
-function git_untracked {
+git_untracked() {
   [[ `git ls-files -o --exclude-standard` != '' ]]
 }
 
-function git_staged {
+git_staged() {
   [[ `git diff --staged --name-only` != '' ]]
 }
 
-function git_dirty {
+git_dirty() {
   git_modified || git_untracked || git_staged
 }
 
-#function symbol {
-  #local result=''
-
-  #if git_modified; then
-    #result='✗'
-  #fi
-
-  #if git_untracked; then
-    #result="${result} ✭"
-  #fi
-
-  #if git_staged; then
-    #result="${result} ∆"
-  #fi
-
-  #echo ${result}
-#}
-
-function git_branch_color {
+git_branch_color() {
   if git_repo; then
     if git_dirty; then
       echo $RED
@@ -50,19 +32,22 @@ function git_branch_color {
   fi
 }
 
-function git_branch {
+git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-function ruby_version {
+ruby_version() {
   echo `~/.rvm/bin/rvm-prompt`
 }
 
-print_pre_prompt () {
+print_pre_prompt() {
   printf "\e[0;34m%$(($COLUMNS))s" $(ruby_version)
+}
+
+command_state() {
+  echo "\$(if [[ \$? == 0 ]]; then echo \"\[\033[0;32m\]☺ \"; else echo \"\[\033[0;31m\]☹ \"; fi)\[\033[00m\]"
 }
 
 PROMPT_COMMAND=print_pre_prompt
 export CLICOLOR=1
-#export PS1="\[\033[\$(echo $YELLOW)m\]\w \[\033[\$(git_branch_color)m\]\$(git_branch) \$(symbol) \[\033[00m\]$\[\033[00m\] "
-export PS1="\[\033[\$(echo $YELLOW)m\]\w \[\033[\$(git_branch_color)m\]\$(git_branch)\[\033[00m\] ۞ \[\033[00m\] "
+export PS1="$(command_state) \[\033[\$(echo $YELLOW)m\]\w \[\033[\$(git_branch_color)m\]\$(git_branch)\[\033[00m\] ۞ \[\033[00m\] "
