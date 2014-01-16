275042886bbfc23dd63cf2628c8a9c6c55bddb15
Jamal El Milahi <jamal@elmilahi.com>
2014-01-16 07:13:43 -0800

    Update Ruby collection script

--- bash/ruby_gc.sh
+++ bash/ruby_gc.sh
@@ -1,3 +1,3 @@
 # http://fredwu.me/post/60441991350/protip-ruby-devs-please-tweak-your-gc-settings-for
 export RUBY_GC_MALLOC_LIMIT=90000000
-export RUBY_FREE_MIN=200000
+export RUBY_GC_HEAP_FREE_SLOTS=200000
