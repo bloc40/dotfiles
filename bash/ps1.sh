e99379208be463149113d16fccb8cf4bf7d98819
Jamal El Milahi <jamal@elmilahi.com>
2014-05-19 07:12:44 -0700

    Remove the last character from the PS1 prompt

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -54,4 +54,4 @@ command_state() {
 
 PROMPT_COMMAND=print_pre_prompt
 export CLICOLOR=1
-export PS1="$(command_state) \[\033[\$(echo $YELLOW)m\]\w \[\033[\$(git_branch_color)m\]\$(git_branch)\[\033[00m\] ۞ \[\033[00m\] "
+export PS1="$(command_state) \[\033[\$(echo $YELLOW)m\]\w \[\033[\$(git_branch_color)m\]\$(git_branch) \[\033[00m\]"
