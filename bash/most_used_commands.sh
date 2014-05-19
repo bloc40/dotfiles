b69b48b6b9584acc1bb971ca6955e6e15d62eb5f
Jamal El Milahi <jamal@elmilahi.com>
2014-05-19 07:09:22 -0700

    More aliases

--- bash/most_used_commands.sh
+++ bash/most_used_commands.sh
@@ -1,5 +1,6 @@
 # List the most used commands
 function most {
   history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
+  #------- Another version ---------------
   #history | awk '{print $2}' | awk 'BEGIN {FS="|"} {print $1}' | sort | uniq -c | sort -r | head
 }
