f70696f94ceb5b0302f47b428ca2a37c033bec12
Jamal El Milahi <jamal@elmilahi.com>
2015-07-21 20:47:15 -0700

    Fix PATH

--- bash/paths.sh
+++ bash/paths.sh
@@ -1,1 +1,1 @@
-PATH=$HOME/bin:"$(brew --prefix coreutils)/libexec/gnubin":/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/heroku/bin:~/.bash/bin:$PATH:$HOME/.rvm/bin
+PATH=$HOME/bin:"$(brew --prefix coreutils)/libexec/gnubin":/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/heroku/bin:~/.bash/bin:$PATH:$HOME/.rvm/bin:$PATH
