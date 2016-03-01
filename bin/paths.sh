aa4a671b3168dad37c344418c70c0aa5b2e73089
Jamal El Milahi <jmilahi@yuzu.com>
2016-03-01 11:23:39 -0800

    [yuzu] Add Go to the path

--- bin/paths.sh
+++ bin/paths.sh
@@ -2,6 +2,5 @@ PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:$PATH
 
 export GOPATH=$HOME/Code/GoLang
 export PATH=$PATH:$GOPATH/bin
-
-# export GOROOT=/usr/local/bin/go
-# export PATH=$PATH:$GOROOT/bin
+export GOROOT=/opt/boxen/homebrew/opt/go/libexec
+export PATH=$PATH:$GOROOT/bin
