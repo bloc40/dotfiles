e8f317785cf4f827734364770ff7c388c42b607c
Jamal El Milahi <jmilahi@yuzu.com>
2016-03-03 09:39:39 -0800

    [yuzu] Change the path for Go

--- bin/paths.sh
+++ bin/paths.sh
@@ -1,6 +1,6 @@
 PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:$PATH
 
-export GOPATH=$HOME/Code/GoLang
+export GOPATH=$HOME/Code/_Go
 export PATH=$PATH:$GOPATH/bin
 export GOROOT=/opt/boxen/homebrew/opt/go/libexec
 export PATH=$PATH:$GOROOT/bin
