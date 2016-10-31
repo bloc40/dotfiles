b3bec5f40203243cf16c7efd792f43f54c5641f9
Jamal El Milahi <jamal@elmilahi.com>
2016-10-31 10:47:11 -0700

    Update the path for Postgres app after upgrading it

--- bin/paths.sh
+++ bin/paths.sh
@@ -1,7 +1,7 @@
 PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:$PATH
 
 export PATH=$PATH:/usr/local/heroku/bin
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.6/bin
 
 export GOPATH=$HOME/Code/_Go
 export PATH=$PATH:$GOPATH/bin
